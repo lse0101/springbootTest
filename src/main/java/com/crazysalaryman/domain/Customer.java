d516f2a5d8cf5411b233f99ac8be23dbf42612ab
lse <lse0101@gmail.com>
2017-01-31 17:53:41 +0900

    JPA로 변경

--- src/main/java/com/crazysalaryman/domain/Customer.java
+++ src/main/java/com/crazysalaryman/domain/Customer.java
@@ -4,14 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 /**
  * Created by lse0101 on 2017-01-23.
  */
+@Entity
+@Table(name="customers")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Customer {
+    @Id
+    @GeneratedValue
     private Integer id;
+
+    @Column(nullable = false)
     private String firstName;
+
+    @Column(nullable = false)
     private String lastName;
 }
