cecdcbb4cd6c7b2a71ec10cf45fa2f62a6b956c5
lse <lse0101@gmail.com>
2017-01-31 12:50:12 +0900

    SQL로그를 더 자세하게 보기위한 Log4jdbc Remix적용 (하지만 여전히 intellij에서는 datasourceProperties 오브젝트를 wired할수 없다고 에러표시함)

--- src/main/java/com/crazysalaryman/App.java
+++ src/main/java/com/crazysalaryman/App.java
@@ -1,45 +1,42 @@
 package com.crazysalaryman;
 
+import com.crazysalaryman.domain.Customer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 /**
  * Created by lse0101 on 2017-01-13.
  */
 
-//@EnableAutoConfiguration
-//@ComponentScan
-    @SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
+@Slf4j
 public class App implements CommandLineRunner{
-//    @Autowired
-//    NamedParameterJdbcTemplate jdbcTemplate;
     @Autowired
-    DataSourceProperties dataSourceProperties;
+    NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println(
-            this.dataSourceProperties.getUrl()
+        String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("id", 1);
+
+        Customer result = jdbcTemplate.queryForObject(sql,
+                                                    param,
+            (rs, rowNum) -> new Customer(rs.getInt("id"),
+                                        rs.getString("first_name"),
+                                        rs.getString("last_name"))
+
+
         );
-//        String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
-//        SqlParameterSource param = new MapSqlParameterSource()
-//                .addValue("id", 1);
-//
-//        Customer result = jdbcTemplate.queryForObject(sql,
-//                                                    param,
-//            (rs, rowNum) -> new Customer(rs.getInt("id"),
-//                                        rs.getString("first_name"),
-//                                        rs.getString("last_name"))
-//
-//
-//        );
-//
-//        System.out.println(result);
+        log.info(result.toString());
     }
 
     public static void main(String[] args) {
--- src/main/java/com/crazysalaryman/AppConfig.java
+++ src/main/java/com/crazysalaryman/AppConfig.java
@@ -5,7 +5,6 @@ import net.sf.log4jdbc.Log4jdbcProxyDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -24,25 +23,55 @@ public class AppConfig {
   DataSourceProperties dataSourceProperties;
   DataSource dataSource;
 
-  @Bean
-  @ConfigurationProperties("spring.datasource")
-  DataSource realDataSource(){
-//    DataSourceBuilder factory = DataSourceBuilder
-//        .create(this.dataSourceProperties.getClassLoader())
-//        .url(this.dataSourceProperties.getUrl())
-//        .username(this.dataSourceProperties.getUsername())
-//        .password(this.dataSourceProperties.getPassword());
-    System.out.println(this.dataSourceProperties.getUrl());
-    DataSourceBuilder factory = DataSourceBuilder.create();
+//  @Bean
+//  @ConfigurationProperties(prefix = "spring.datasource")
+//  DataSourceProperties dataSourceProperties(){
+//    DataSourceProperties dataSourceProperties = new DataSourceProperties();
+//    return dataSourceProperties;
+//  }
 
-    this.dataSource = factory.build();
-    return this.dataSource;
-  }
+//  @Bean
+//  DataSource realDataSource(){
+//    DataSourceBuilder factory = DataSourceBuilder
+//        .create(dataSourceProperties.getClassLoader())
+//        .url(dataSourceProperties.getUrl())
+//        .username(dataSourceProperties.getUsername())
+//        .password(dataSourceProperties.getPassword());
+//
+//    this.dataSource = factory.build();
+//
+//    return this.dataSource;
+//  }
 
   @Bean
   @Primary
   DataSource dataSource(){
-    return new Log4jdbcProxyDataSource(this.dataSource);
+    DataSourceBuilder factory = DataSourceBuilder
+            .create(dataSourceProperties.getClassLoader())
+            .url(dataSourceProperties.getUrl())
+            .username(dataSourceProperties.getUsername())
+            .password(dataSourceProperties.getPassword());
+
+    return new Log4jdbcProxyDataSource(factory.build());
   }
+
+/*  @Bean
+  NamedParameterJdbcTemplate jdbcTemplate(){
+    return new NamedParameterJdbcTemplate(this.dataSource());
+  }*/
+
+/*  @Bean
+  DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+    DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
+
+    dataSourceInitializer.setDataSource(dataSource);
+    ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
+    resourceDatabasePopulator.addScript(new ClassPathResource("schema.sql"));
+    resourceDatabasePopulator.addScript(new ClassPathResource("data.sql"));
+    dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
+    dataSourceInitializer.setEnabled(true);
+
+    return dataSourceInitializer;
+  }*/
 
 }
